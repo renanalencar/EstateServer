75b4e9d78d6383c087313d942f435f0e988782d6
Renan Costa Alencar <renan.costaalencar@gmail.com>
2020-10-24 01:25:06 -0300

    first commit

---
+++ src/br/com/renanalencar/moreaqui/LocationEstate.java
@@ -0,0 +1,21 @@
+package br.com.renanalencar.moreaqui;
+
+public class LocationEstate  extends Estate {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    /** The size of the estate: small, medium, large. */
+    public final Double LATITUDE;
+
+    /** True if the estate is under construction. */
+    public final Double LONGITUDE;
+
+    public LocationEstate(String type, String size, String phone, String inConstruction, double latitude, double longitude) {
+        super(type, size, phone, inConstruction);
+        this.LATITUDE = latitude;
+        this.LONGITUDE = longitude;
+    }
+
+}
